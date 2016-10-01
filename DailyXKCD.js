8bb18f95b38dc4910057c458a87a88af97cf1a51
Derek Solven <dsolven@gmail.com>
2016-10-01 16:31:45 -0700

    Change update scheme from interval to once per day with heartbeat per hour.

--- DailyXKCD.js
+++ DailyXKCD.js
@@ -3,13 +3,13 @@ Module.register("DailyXKCD",{
 	// Default module config.
 	defaults: {
 		dailyJsonUrl : "http://xkcd.com/info.0.json",
-		updateInterval : 1000 * 60 * 60 * 10, // 10 hours
 		invertColors : false,
-		onlyShowTodaysComic: false
+		onlyShowTodaysComic: false,
+		updateTOD: 5, // Time of day to download new comic, 0-23 hours
+		initialDelay: 10 * 1000 // 30 seconds. Initial heartbeat delay.
 	},
 
 	start: function() {
-		Log.info(this.config);
 		Log.info("Starting module: " + this.name);
 
 		this.dailyComic = "";
@@ -18,7 +18,11 @@ Module.register("DailyXKCD",{
 		this.dailyComicDate = moment({y:0,M:0,d:0});
 		this.showComic = false;
 
-		this.getComic();
+		var now = moment();
+		this.nextComicDownloadTime = moment([now.year(), now.month(), now.date() +1, this.config.updateTOD]); //Tomorrow at 5AM
+
+		this.getComic(); // Retreive a comic right away on startup
+		this.scheduleHeartbeat(moment().add(this.config.initialDelay,'ms'));
 	},
 
 	// Define required scripts.
@@ -27,12 +31,13 @@ Module.register("DailyXKCD",{
 	},
 
 	getComic: function() {
-		Log.info("XKCD: Getting comic.");
+		Log.info("XKCD: Getting comic. SendSocketNotification GET_COMIC.");
 
 		this.sendSocketNotification("GET_COMIC", {config: this.config});
 	},
 
 	socketNotificationReceived: function(notification, payload) {
+		Log.log(this.name + " received a socket notification: " + notification + " - Payload: " + payload);
 		if(notification === "COMIC"){
 				Log.info(payload.img);
 				this.dailyComic = payload.img;
@@ -43,8 +48,7 @@ Module.register("DailyXKCD",{
 					month: payload.month -1,
 					day: payload.day});
 				this.showComic = this.config.onlyShowTodaysComic ? this.isComicFromToday() : true;
-				this.scheduleUpdate();
-				Log.log(this);
+				this.updateDom(2000);
 		}
 	},
 
@@ -82,14 +86,40 @@ Module.register("DailyXKCD",{
 		}
 	},
 
-	scheduleUpdate: function() {
-		var self = this;
+	scheduleHeartbeat: function(whenToCheck) {
+		// Heartbeat to check if it is time to download new comic periodically
 
-		self.updateDom(2000);
+		// Calculate time until next beat
+		var durationUntilNextCheck = moment.duration(whenToCheck.diff(moment()));
+		Log.group('xkcd: scheduling heartbeat check for ' + durationUntilNextCheck.humanize(true) + ' from now.');
+		Log.log('xkcd: Current time: ' + moment().toString());
+		Log.log('xkcd: Time to check next: ' + whenToCheck.toString());
+		Log.groupEnd();
 
-		setInterval(function() {
-			self.getComic();
-		}, this.config.updateInterval);
+		setTimeout(this.updateComic, durationUntilNextCheck, this);
+	},
+
+	updateComic: function(self) {
+		// If it is time to download a new comic, do so.
+
+		// This is only accessed as a callback function. Needs to have context specified.
+		Log.log('xkcd: entering updateComic()');
+
+		if (moment().isSameOrAfter(self.nextComicDownloadTime)) {
+			Log.log('xkcd: Yes, go get comic');
+			self.getComic()
+
+			var now = moment();
+			self.nextComicDownloadTime = moment([now.year(), now.month(), now.date() +1, self.config.updateTOD]); //Tomorrow at 5AM
+		} else {
+			Log.log('xkcd: Not ' + self.nextComicDownloadTime.toString() + ' yet, don\'t download new comic');
+		}
+
+		 // Check again one hour from now, on the hour.
+		 // TODO: May need to re-evaluate if this is running correctly. For now, just log timestamps.
+		var now = moment();
+		var whenToCheckNext = moment([now.year(), now.month(), now.date(), now.hour() + 1]);
+		self.scheduleHeartbeat(whenToCheckNext);
 	}
 
 });
